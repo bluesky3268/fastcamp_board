e44df41bc42f6dd4b01bcf8b7643535e0bb7f385
HYUNBIN CHO <bluesky3268@gmail.com>
2023-01-27 23:05:50 +0900

    #13 [enhancement] - Auditing Fields 추출
    * 엔티티 클래스에 중복으로 들어가는 생성자, 생성일시, 수정자, 수정일시는 도메인과 직접적인 연관이 없는 요소이기 때문에 @MappedSuperClass를 이용하여 상속으로 추출함
    
    * @DateTimeFormat 요소 추가 및 생성일시, 수정일시에 jpa옵션 추가(updatable)

--- src/main/java/com/fastcapmus/board/domain/ArticleComment.java
+++ src/main/java/com/fastcapmus/board/domain/ArticleComment.java
@@ -4,13 +4,7 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import java.time.LocalDateTime;
 import java.util.Objects;
 
 @Getter
@@ -20,9 +14,8 @@ import java.util.Objects;
         @Index(columnList = "createdBy"),
         @Index(columnList = "createdAt")
 })
-@EntityListeners(AuditingEntityListener.class)
 @Entity
-public class ArticleComment {
+public class ArticleComment extends AuditingFields{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -35,22 +28,6 @@ public class ArticleComment {
     @Setter
     @Column(nullable = false, length = 500)
     private String content; // 내용
-
-    @CreatedBy
-    @Column(nullable = false, length = 100)
-    private String createdBy; // 생성자
-
-    @CreatedDate
-    @Column(nullable = false)
-    private LocalDateTime createdAt; // 생성일시
-
-    @LastModifiedBy
-    @Column(nullable = false, length = 100)
-    private String modifiedBy; // 수정자
-
-    @LastModifiedDate
-    @Column(nullable = false)
-    private LocalDateTime modifiedAt; // 수정일시
 
     protected ArticleComment() {
     }
